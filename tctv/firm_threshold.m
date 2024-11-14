762a573d55cda8857f9727beaee8cbe83c41ed33
XHB <74241665+xhb-61@users.noreply.github.com>
2024-11-14 12:24:20 +0800

    Delete tctv directory

--- tctv/firm_threshold.m
+++
@@ -1,5 +0,0 @@
-function x = firm_threshold(x, value_low, value_high)
-x(abs(x) <= value_low) = 0;
-idx = find(value_low < abs(x) & abs(x) <= value_high);
-x(idx) = x(idx) .* value_high .* (1 - value_low ./ x(idx)) ./ (value_high- value_low);
-end
